18a22ddb9cfae68f131ee43559485d9455ae2d39
yamat47 <yamat47.thirddown@gmail.com>
2025-09-05 08:55:02 +0900

    Add random query

--- app/models/aws_service.rb
+++ app/models/aws_service.rb
@@ -3,4 +3,10 @@
 class AwsService < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  class << self
+    def random
+      order('RANDOM()').first
+    end
+  end
 end
